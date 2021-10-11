b39af282efc742268df0477ce1be796df52a33d4
kestutisvirbickas <v.kestutis123@gmail.com>
2021-10-11 23:22:20 +0300

    Change code spacing

--- client/src/components/VideoContainer/VideoSearch/SearchOrLoadVideosInput.js
+++ client/src/components/VideoContainer/VideoSearch/SearchOrLoadVideosInput.js
@@ -3,7 +3,8 @@ import { InputGroup, InputRightElement, Input, Button } from "@chakra-ui/react";
 import { SocketContext } from "../../../store/contexts/SocketContext";
 import { getVideosInfoURL } from "../../../shared/constants";
 
-const validYoutubeLink = /^(?:https?:)?(?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]{7,15})(?:[\?&][a-zA-Z0-9\_-]+=[a-zA-Z0-9\_-]+)*$/;
+const validYoutubeLink =
+  /^(?:https?:)?(?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]{7,15})(?:[\?&][a-zA-Z0-9\_-]+=[a-zA-Z0-9\_-]+)*$/;
 
 const SearchOrLoadVideosInput = ({ setFoundVideos }) => {
   const { state } = useContext(SocketContext);
@@ -14,17 +15,23 @@ const SearchOrLoadVideosInput = ({ setFoundVideos }) => {
 
   const searchVideos = async (searchInput) => {
     if (!searchInput) return;
+
     setIsLoading(true);
+
     try {
       const res = await fetch(getVideosInfoURL(searchInput));
+
       if (!res.ok) {
         throw new Error(res.error);
       }
+
       const data = await res.json();
+
       setFoundVideos([...data.items]);
     } catch (err) {
       console.error(err);
     }
+
     setIsLoading(false);
   };
 
@@ -34,6 +41,7 @@ const SearchOrLoadVideosInput = ({ setFoundVideos }) => {
     } else {
       searchVideos(input);
     }
+
     setVideoSearchInputOrURL("");
   };
 
